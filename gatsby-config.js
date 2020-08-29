de0ce880b2116fe16083e016e37cbee4c46d32eb
teamzz111 <felipeflogxd@gmailcom>
2020-08-29 16:18:34 -0500

    siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Lifestyle Shopping`,
+    author: {
+      name: `Team Queso`,
+      summary: `who lives and works in San Francisco building useful things.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    social: {
+      twitter: `kylemathews`,
+    },
+  },
   plugins: [
     "gatsby-plugin-tslint",
     "gatsby-plugin-typescript",
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Home: React.FC = () => {
+  return <p>hola</p>;
+};
+
+export default Home;
