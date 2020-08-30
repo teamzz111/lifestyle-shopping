073678583aecb507eea56d6bb7dea69a222fb1c5
teamzz111 <felipeflogxd@gmailcom>
2020-08-29 22:23:05 -0500

    sub menu categories and palette

---
+++ src/pages/resources.ts
@@ -0,0 +1,22 @@
+export const categoryOptions: IOption[] = [
+  {
+    id: 1,
+    value: 'Fashion',
+  },
+  {
+    id: 2,
+    value: 'Electrónica',
+  },
+  {
+    id: 3,
+    value: 'Miscelaneos',
+  },
+  {
+    id: 4,
+    value: 'Coleccionables',
+  },
+  {
+    id: 5,
+    value: 'Bicicletas',
+  },
+];
